98a4123ac71a7c0a9f45fdc9bc0cbe38b470bc69
lapinmark <51202075+lapinmark@users.noreply.github.com>
2019-12-03 13:49:37 +0300

    reactivity

--- Dice/js/VueDice.js
+++ Dice/js/VueDice.js
@@ -4,6 +4,22 @@ let diceSample = new Vue({
     maxValue: 6,
     minValue: 1
   },
+
+  computed: {
+    diceClass: function () {
+      diceValue = this.randomDice();
+
+      return {
+        'dice_one': diceValue === 1,
+        'dice_two': diceValue === 2,
+        'dice_three': diceValue === 3,
+        'dice_four': diceValue === 4,
+        'dice_five': diceValue === 5,
+        'dice_six': diceValue === 6
+      }
+    }
+  },
+
   methods: {
     randomDice() {
       let value = this.minValue + Math.random() * (this.maxValue + 1 - this.minValue);
@@ -12,36 +28,7 @@ let diceSample = new Vue({
 
     stopRotate(dice) {
       dice.classList.remove('dice_rotate');
-    },
-
-    changeValue() {
-      let dice = document.getElementById('dice');
-      dice.classList.add('dice_rotate');
-      dice.classList.remove('dice_one', 'dice_two','dice_three', 'dice_four','dice_five', 'dice_six');
-      diceValue = this.randomDice();
-
-      switch(diceValue) {
-        case 1: 
-          dice.classList.add('dice_one');
-          break;
-        case 2: 
-          dice.classList.add('dice_two');
-          break;
-        case 3: 
-          dice.classList.add('dice_three');
-          break;
-        case 4: 
-          dice.classList.add('dice_four');
-          break;
-        case 5: 
-          dice.classList.add('dice_five');
-          break;
-        case 6: 
-          dice.classList.add('dice_six');
-          break;
-      };
-
-      setTimeout(this.stopRotate, 1100, dice);
     }
+    
   }
 });
