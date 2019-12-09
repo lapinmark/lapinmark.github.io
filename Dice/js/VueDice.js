23d748af12c28c74824e6bd1e84a683b8f2af5e7
lapinmark <51202075+lapinmark@users.noreply.github.com>
2019-12-09 15:18:02 +0300

    reactivity + rotate

--- Dice/js/VueDice.js
+++ Dice/js/VueDice.js
@@ -1,80 +1,65 @@
-let diceSample = new Vue({
+const diceDefaultValue = 6;
+const rotateDefaultValue = '';
+
+const diceSample = new Vue({
   el: '.dice-sample',
+  template: `
+  <div class="container dice-sample" @click="shuffleDice">
+        <div class="dice" id="dice" :class="[diceClass, rotateClass]">
+            <div class="dot dot_top-left"></div>
+            <div class="dot dot_middle-left"></div>
+            <div class="dot dot_bottom-left"></div>
+            <div class="dot dot_center"></div>
+            <div class="dot dot_top-right"></div>
+            <div class="dot dot_middle-right"></div>
+            <div class="dot dot_bottom-right"></div>
+        </div>
+    </div>
+  `,
   data: {
     maxValue: 6,
-    minValue: 1
+    minValue: 1,
+    diceValue: diceDefaultValue,
+    rotateClass: rotateDefaultValue
   },
 
   computed: {
-    diceClass: {
-
-      get: function () {
-        diceValue = this.randomDice();
-
-        switch (diceValue) {
-          case 1:
-            diceClassValue = 'dice_one';
-            break;
-          case 2:
-            diceClassValue = 'dice_two';
-            break;
-          case 3:
-            diceClassValue = 'dice_three';
-            break;
-          case 4:
-            diceClassValue = 'dice_four';
-            break;
-          case 5:
-            diceClassValue = 'dice_five';
-            break;
-          case 6:
-            diceClassValue = 'dice_six';
-            break;
-        }
-
-        return diceClassValue;
-
-      },
-
-      set: function () {
-        diceValue = this.randomDice();
-
-        switch (diceValue) {
-          case 1:
-            diceClassValue = 'dice_one';
-            break;
-          case 2:
-            diceClassValue = 'dice_two';
-            break;
-          case 3:
-            diceClassValue = 'dice_three';
-            break;
-          case 4:
-            diceClassValue = 'dice_four';
-            break;
-          case 5:
-            diceClassValue = 'dice_five';
-            break;
-          case 6:
-            diceClassValue = 'dice_six';
-            break;
-        }
+    diceClass() {
+      switch (this.diceValue) {
+        case 1:
+          return 'dice_one';
+        case 2:
+          return 'dice_two';
+        case 3:
+          return 'dice_three';
+        case 4:
+          return 'dice_four';
+        case 5:
+          return 'dice_five';
+        case 6:
+          return 'dice_six';
       }
     }
   },
 
   methods: {
-    randomDice() {
+    getRandomDice() {
       let value = this.minValue + Math.random() * (this.maxValue + 1 - this.minValue);
       return Math.floor(value);
     },
 
-    stopRotate(dice) {
-      dice.classList.remove('dice_rotate');
+    stopRotation() {
+      this.rotateClass = '';
     },
 
-    changeDiceValue() {
-      diceSample.diceClass = '';
+    rotateDice() {
+      this.rotateClass = 'dice_rotate';
+      setTimeout(this.stopRotation, 1100)
+    },
+
+    shuffleDice() {
+      this.rotateDice();
+      this.diceValue = this.getRandomDice();
     }
   }
 });
