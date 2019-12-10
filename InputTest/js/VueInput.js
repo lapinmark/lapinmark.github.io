795d67af858f46f0af08d2a3cc72fd6f60fa7578
lapinmark <51202075+lapinmark@users.noreply.github.com>
2019-12-10 19:17:49 +0300

    upd

--- InputTest/js/VueInput.js
+++ InputTest/js/VueInput.js
@@ -1,11 +1,16 @@
 const diceSample = new Vue({
   el: '.input-test',
   data: {
+    query: 'Roboto',
     fonts: ['Roboto Slab', 'Roboto Condensed', 'Roboto Mono']
   },
 
   computed: {
-
+    searchedFonts(query) {
+      return this.fonts.filter(function(currentFont) {
+        return currentFont.toLowerCase().indexOf(query.toLowerCase()) > -1;
+    })
+    }
   },
 
   methods: {
