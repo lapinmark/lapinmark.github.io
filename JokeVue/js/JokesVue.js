0aa76bec2195568a683032a29f0720774c2a9667
lapinmark <51202075+lapinmark@users.noreply.github.com>
2021-09-05 16:20:17 +0300

    Добавлены лайки
    Добавлены лайки, информация о них хранится в localStorage

--- JokeVue/js/JokesVue.js
+++ JokeVue/js/JokesVue.js
@@ -3,11 +3,13 @@ const JokesApp = new Vue({
 
   data: {
     jokesList: [],
-    search: ''
+    search: '',
+    likedJokes: []
   },
 
   async created() {
     this.jokesList = await this.getJokes()
+    this.copyStorage()
   },
 
   computed: {
@@ -28,6 +30,36 @@ const JokesApp = new Vue({
       const response = await fetch("https://v2.jokeapi.dev/joke/Any?amount=10")
       const data = await response.json()
       return data.jokes
+    },
+
+    copyStorage() {
+      const response = localStorage.getItem('likedJokes')
+      this.likedJokes = JSON.parse(response) ?? []
+    },
+
+    likeJoke(id) {
+      const response = localStorage.getItem('likedJokes')
+      const array = JSON.parse(response) ?? []
+      const index = array.indexOf(id)
+      if (index === -1) {
+        array.push(id)
+      }
+      else {
+        array.splice(index, 1)
+      }
+      this.likedJokes = array
+      const string = JSON.stringify(array)
+      localStorage.setItem('likedJokes', string)
+    },
+
+    getButtonClass(id) {
+      const index = this.likedJokes.indexOf(id)
+      if (index === -1) {
+        return 'jokes__like_neutral'
+      }
+      else {
+        return 'jokes__like_active'
+      }
     }
   }
 })
